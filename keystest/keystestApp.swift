bbaeea241d8991d7f196b906c7408681d7d5d3e1
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-03-23 11:08:21 +0100

    Initial Commit

---
+++ keystest/keystestApp.swift
@@ -0,0 +1,17 @@
+//
+//  keystestApp.swift
+//  keystest
+//
+//  Created by Till Hemmerich on 23.03.23.
+//
+
+import SwiftUI
+
+@main
+struct keystestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
