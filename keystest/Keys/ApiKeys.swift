9b576baa83620d3eb0ed7c7bf1515fa5eec26789
Till-Hemmerich-Syntax <till.hemmerich@syntax-institut.de>
2023-03-23 11:18:36 +0100

    asd

---
+++ keystest/Keys/ApiKeys.swift
@@ -0,0 +1,13 @@
+//
+//  ApiKeys.swift
+//  keystest
+//
+//  Created by Till Hemmerich on 23.03.23.
+//
+
+import Foundation
+
+
+struct ApiKeys {
+    static let apiKey : String = "here goes your api key"
+}
